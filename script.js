9a9d271e7bc02fb8a1ed367dfb35d51bff63e967
tl51829 <135583181+tl51829@users.noreply.github.com>
2023-08-24 22:47:37 +0800

    Add event listeners and logic so basic calculations can be done

--- script.js
+++ script.js
@@ -17,28 +17,62 @@ function divide(a, b) {
 function operate(number1, operator, number2) {
     switch (operator) {
         case "+":
-            console.log(add(number1, number2));
-            break;
+            return add(number1, number2);
         case "-":
-            console.log(subtract(number1, number2));
-            break;
+            return subtract(number1, number2);
         case "*":
-            console.log(multiply(number1, number2));
-            break;
+            return multiply(number1, number2);
         case "/":
-            console.log(divide(number1, number2));
-            break;
+            return divide(number1, number2);
     }
 }
 
-let number1, operator, number2;
+let number1 = null;
+let operator = null;
+let number2 = null;
+
 const screen = document.getElementById("screen");
 const buttons = document.querySelectorAll("button");
 
 let display;
+let clicked;
+let result;
+
+// Remember to round results so it doesn't overflow
 buttons.forEach(button => {
     button.addEventListener('click', e => {
+        clicked = button.value;
+        if (clicked == "=") {
+            if (operator != null && number2 != null && number1 != null) {
+                result = operate(Number(number1), operator, Number(number2));
+                screen.textContent += "=" + result;
+                operator = null;
+                number1 = result;
+                number2 = null;
+            }
+        } else if (clicked == "D") {
+            
+        } else if (clicked == "C") {
+            
+        } else if (!isNaN(clicked) || clicked == ".") {
+            if (operator == null) {
+                (number1 == null) ? (number1 = clicked) : (number1 += clicked); 
+            } else {
+                (number2 == null) ? (number2 = clicked) : (number2 += clicked);          
+            }
+        } else {
+            if (number1 != null && number2 != null) {
+                result = operate(Number(number1), operator, Number(number2));
+                screen.textContent += '=' + result;
+                number1 = result;
+                number2 = null;
+            }
+            operator = clicked;
+        } 
+
         display = button.textContent;
-        screen.textContent += display;
-    })
-})
+        if (display != "=") {
+            screen.textContent += display;
+        }
+    });
+});
