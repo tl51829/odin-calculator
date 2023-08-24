e883621b8d48ef255838ca70349a461135037a9d
tl51829 <135583181+tl51829@users.noreply.github.com>
2023-08-24 16:40:17 +0800

    Add function to decide which operation to use

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(number1, operator, number2) {
+    switch (operator) {
+        case "+":
+            console.log(add(number1, number2));
+            break;
+        case "-":
+            console.log(subtract(number1, number2));
+            break;
+        case "*":
+            console.log(multiply(number1, number2));
+            break;
+        case "/":
+            console.log(divide(number1, number2));
+            break;
+    }
+}
+
+let number1, operator, number2;
